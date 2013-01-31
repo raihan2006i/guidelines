762bee3eed1882f400c6c1fc54c9c20737e4d43b
Tessa Davis <tessadavis06@gmail.com>
2013-01-31 22:56:30 +1100

    Updated profile page and added scoped status updates to it

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Guidelines::Application.routes.draw do
   get 'guidelines', to: 'guidelines#index', as: :guidelines
   root :to => 'guidelines#index'
 
-
+  get '/:id', to: 'profiles#show'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- test/functional/profiles_controller_test.rb
+++ test/functional/profiles_controller_test.rb
@@ -2,8 +2,29 @@ require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, id: users(:tessa).profile_name
     assert_response :success
+    assert_template 'profiles/show'
   end
 
+
+	test "should render a 404 on profile not found" do
+	  	get :show, id: "doesan't exist"
+	  	assert_response :not_found
+
+	end
+
+	test "that variables are assigned on show page" do 
+		get :show, id: users(:tessa).profile_name
+    	assert assigns (:user)
+    	assert_not_empty assigns(:guidelines)
+
+	end
+
+	test "only shows the correct user's guidelines" do
+		get :show, id: users(:tessa).profile_name
+    	assigns(:guidelines).each do |guideline|
+    		assert_equal users(:tessa), guideline.user
+    	end
+	end
 end
