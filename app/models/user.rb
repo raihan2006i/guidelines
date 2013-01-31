0eb816cfff7a063c526dcf4822bd76a83eaadeba
Tessa Davis <tessadavis06@gmail.com>
2013-01-31 13:02:38 +1100

    added tests for first name, last and profile name correctness

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,17 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :first_name, :last_name, :profile_name, :base_hospital, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  validates :first_name, presence:true
+  validates :last_name, presence:true
+  validates :profile_name, presence:true,
+  						uniqueness:true,
+              format: {
+                with: /a-zA-Z0-9_-/,
+                message: 'Must be formatted correctly.'
+              }
+  validates :base_hospital, presence:true
+
  has_many :guidelines
 
  def full_name
