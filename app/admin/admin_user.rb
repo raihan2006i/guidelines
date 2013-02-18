3463cfb31a8e08e85d470320830d8db17b8cde05
Tessa Davis <tessadavis06@gmail.com>
2013-02-18 17:04:37 +1100

    sorted activeadmin

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -9,12 +9,12 @@ ActiveAdmin.register AdminUser do
 
   filter :email                       
 
-  form do |f|                         
-    f.inputs "Admin Details" do       
-      f.input :email                  
-      f.input :password               
-      f.input :password_confirmation  
-    end                               
-    f.actions                         
-  end                                 
+
+  form do |f|
+    f.inputs "Admin Details" do
+      f.input :email
+    end
+    f.buttons
+  end
+end                                
 end                                   
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -8,4 +8,11 @@ class AdminUser < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+before_destroy :raise_if_last
+def raise_if_last
+  if AdminUser.count < 2
+    raise "Can't delete last admin user"
+  end
+end
 end
