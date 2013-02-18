1770103810e13c0c1545b5fddeb0624fc1535c69
Tessa Davis <tessadavis06@gmail.com>
2013-02-18 18:44:38 +1100

    added new button on show page

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -8,7 +8,11 @@ class AdminUser < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
-
+  
+after_create { |admin| admin.send_reset_password_instructions }
+  def password_required?
+    new_record? ? false : super
+  end
 before_destroy :raise_if_last
 def raise_if_last
   if AdminUser.count < 2
