867372754f78e91323e76f1e148b4b1e612fa4b5
Tessa Davis <tessadavis06@gmail.com>
2013-03-13 12:26:52 +1100

    sorted redirect after admin login

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,6 @@
 class AdminController < ApplicationController	
+
+	def after_sign_in_path_for(resource)
+	 admin_dashboard
+	end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
 
+
+
 	include PublicActivity::StoreController
 	  protect_from_forgery
+
 
 	def after_sign_in_path_for(resource)
 	 favourites_path
